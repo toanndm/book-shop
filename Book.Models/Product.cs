4d06fdeaf21e58079c60496bd48c1a6925f4cb81
Nguyen Toan <toanndm@gmail.com>
2023-12-27 11:29:06 +0700

    Add table Product and seed data

---
+++ Book.Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public String ISBN { get; set; }
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public Double ListPrice { get; set; }
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public Double Price { get; set; }
+        [Required]
+        [Display(Name = "Price for 50-100")]
+        [Range(1, 1000)]
+        public Double Price50 { get; set; }
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public Double Price100 { get; set; }
+    }
+}
