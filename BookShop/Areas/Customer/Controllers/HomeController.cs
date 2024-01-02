fa000c8dd74daff042fd48af059eb4ca9208654e
Nguyen Toan <toanndm@gmail.com>
2024-01-02 12:06:46 +0700

    Complete HomePage

--- BookShop/Areas/Customer/Controllers/HomeController.cs
+++ BookShop/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Book.DataAccess.Repository.IRepository;
 using Book.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,18 @@ namespace BookShop.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProp:"Category");
+            return View(products);
         }
 
         public IActionResult Privacy()
