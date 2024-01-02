1f7e7c74d2a39af9c00566a31e10e11cb6875bf7
Nguyen Toan <toanndm@gmail.com>
2024-01-02 23:39:00 +0700

    Fix bug middleware

--- BookShop/Program.cs
+++ BookShop/Program.cs
@@ -38,9 +38,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
+
+
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
